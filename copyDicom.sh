622df274bfaee9cb1136e369c02324d934093840
Elijah Hudlow <hudlowe@terminator.wpic.upmc.edu>
2024-10-24 15:25:25 -0400

    major changes to run more like copyDicom-onesession

--- copyDicom.sh
+++ copyDicom.sh
@@ -1,41 +1,25 @@
 #!/usr/bin/bash
-# Copy the first DICOM file from rest, task, and dwi folders
-
-rest_folder_a=(/Volumes/Hera/Raw/MRprojects/Habit/*/*/Resting-state_ME_4*)
-task_folder_a=(/Volumes/Hera/Raw/MRprojects/Habit/*/*/HabitTask_704*)
-dwi_folder_a=(/Volumes/Hera/Raw/MRprojects/Habit/*/*/dMRI_b0_AP_1*)
-
-rest_folder_b=(/Volumes/Hera/Raw/MRprojects/Habit/*/*/Resting-state_ME_repeat_4*)
-task_folder_b=(/Volumes/Hera/Raw/MRprojects/Habit/*/*/H)
-dwi_folder_b=(/Volumes/Hera/Raw/MRprojects/Habit/*/*/dMRI_dir98-1_PA_1*)
-
-output_dir="$HOME/src/mrrc-hdr-qa/dicoms"
+#Copy the first two DICOM files from rest, task, and dwi folders
+#
+output_dir="$HOME/src/mrrc-hdr-qa/dicoms/"
 
 mkdir -p "$output_dir"
 
-copy_first_dicom() {
-    local folders=("$@")
-    local count=0
-    for d in "${folders[@]}"; do
-	if [ "$count" -ge 2 ]; then
-		break
-	fi
-        # Find and copy the first DICOM file to output directory
-        find "$d" -maxdepth 1 -type f -print -quit |
-        while read -r file; do
-            cp "$file" "$output_dir/$(basename "$file")"
-	    ((count++))
-	
-	    if [ "$count" -ge 2 ]; then
-		    break
-	    fi
-    	done
-    done
+copy_first_two() {
+	local sessions=("$@")
+	for session in "${sessions[@]}"; do
+		# copy the first two dicoms in the session
+		files=($(ls "$session" | head -n2))
+		for file in "${files[@]}"; do
+			cp "$session/$file" "$output_dir/$(basename "$file")"
+		done
+	done
 }
 
-# Copy from rest, task and DWI folders
-copy_first_dicom "${rest_folder_a[@]}"
-copy_first_dicom "${rest_folder_b[@]}"
-copy_first_dicom "${task_folder_a[@]}"
-copy_first_dicom "${dwi_folder_a[@]}"
-copy_first_dicom "${dwi_folder_b[@]}"
+rest_sessions=($(printf "%s\n" /Volumes/Hera/Raw/MRprojects/Habit/2*/1*/Resting-state_ME_4*/ | head -n2))
+task_sessions=($(printf "%s\n" /Volumes/Hera/Raw/MRprojects/Habit/2*/1*/HabitTask_704*/ | head -n2))
+dwi_sessions=($(printf "%s\n" /Volumes/Hera/Raw/MRprojects/Habit/2*/1*/dMRI_b0_AP_1*/ | head -n2))
+
+copy_first_two "${rest_sessions[@]}"
+copy_first_two "${task_sessions[@]}"
+copy_first_two "${dwi_sessions[@]}"
