4ecc51aa1fafb082ef5551973ce7e7b3d1e6014e
Elijah Hudlow <hudlowe@terminator.wpic.upmc.edu>
2024-10-28 15:36:53 -0400

    conflict

--- dcm2nii_check.bash
+++ dcm2nii_check.bash
@@ -1,5 +1,9 @@
 # use dcm2niix to make bids sidecar json ('-b o' is json only, no nifti) files from dicom header
+<<<<<<< HEAD
 # want to check acq dir there in addition to dicom tag in case we're looking at the wrong one
+=======
+# want to check acq dir there in addtion to dicom tag incase we're looking at the wrong one
+>>>>>>> 48cf3dc (first commit for audit.py)
 echo /Volumes/Hera/Raw/MRprojects/Habit/20*-*/1*_2*/dMRI_*/ |
     head -n 15 |
     xargs -I{} -n1 dcm2niix -o example_jsons/ -b o {}
