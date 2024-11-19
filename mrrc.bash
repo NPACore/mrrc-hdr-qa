1c4b9be507a7037e1ae299fc2589190cf65c2869
WillForan <willforan@gmail.com>
2024-11-19 13:38:32 -0500

    cerebro/builddb: per project option and cert issues

--- mrrc.bash
+++ mrrc.bash
@@ -3,14 +3,14 @@
 # 20241026 -init
 
 # paper over centOS7+guix config issues
-export SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt LC_ALL=C
+export SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt LC_ALL=C LOGLEVEL=WARN
 make .venv
 . .venv/bin/activate
 
 log(){ echo "$(date +"[%s] %F %T"):: $*" | tee -a build.log; }
 
 log parse dicoms start
-./00_build_db.bash /disk/mace2/scan_data/*
+./00_build_db.bash /disk/mace2/scan_data/WPC-*
 
 log starting sqlite db
 cat db/*.txt | ./acq2sqlite.py
