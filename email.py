614454d102f004bfd509e8c0bedd0e735deb0b51
WillForan <WillForan@users.noreply.github.com>
2024-11-21 03:58:59 +0000

    Apply automatic changes

--- email.py
+++ email.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-from pyrage import x25519, decrypt # 'age' password encryption
-with open('creds/email.key', 'r') as f:
+from pyrage import decrypt, x25519  # 'age' password encryption
+
+with open("creds/email.key", "r") as f:
     key = f.readlines()[-1].strip()
     ident = x25519.Identity.from_str(key)
-with open('creds/email.age', 'rb') as f:
+with open("creds/email.age", "rb") as f:
     msg = decrypt(f.read(), [ident]).decode()
-    (addr,pswd) = msg.split("\t")
+    (addr, pswd) = msg.split("\t")
 
 # TODO: SMTP send
 print(f"email:'{addr}' pass:'{pswd}'")
