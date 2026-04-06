8ea27520200112ad276a223d019805d9c3ab367a
WillForan <willforan@gmail.com>
2026-04-06 08:24:06 -0400

    wip: todo file and review annotations

---
+++ .reviews/.dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((text-mode . ((annotate-file-buffer-local . t))))
